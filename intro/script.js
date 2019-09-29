be6393a6eb75774a536be95b3579797d6c9da8cc
Gerald Derrick <jerry.derrick@gmail.com>
2019-09-28 21:33:43 -0500

    Initial upload of p1 folder

---
+++ intro/script.js
@@ -0,0 +1,25 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        'playerName': null,
+        'guess': null,
+        'guesses': [],
+        'results': null,
+        'settings': {
+            'max': 200,
+            'type': 'numeric',
+            'guessLimit': 15
+        },
+        'answer': 55,
+        'guessesDetailed': [
+            { guess: 15, result: 'low' },
+            { guess: 60, result: 'high' },
+            { guess: 50, result: 'low' }
+        ]
+    },
+    methods: {
+        submitGuess: function () {
+            this.guesses.push(this.guess);
+        }
+    }
+});
---
+++ p1/script.js
@@ -0,0 +1,6 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        isAdmin: [0, 1, 2, 3]
+    }
+});
