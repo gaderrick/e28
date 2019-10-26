ffd84136ab4045359dedb39758aa50e6874bbee5
Gerald Derrick <jerry.derrick@gmail.com>
2019-10-25 22:05:40 -0500

    week8 and week8spa modifications

--- week8spa/src/App.vue
+++ week8spa/src/App.vue
@@ -1,16 +1,72 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Hello world from week8spa" />
+    <div>
+      <label>
+        <input type="radio" value="heads" v-model="choice" />heads
+      </label>
+      <label>
+        <input type="radio" value="tails" v-model="choice" />tails
+      </label>
+    </div>
+
+    <button id="flipBtn" v-on:click="flip">Flip!</button>
+
+    <round-detail
+      v-for="round in rounds"
+      :number="round.number"
+      :winner="round.winner"
+      :key="round.number"
+      v-on:delete-round="deleteRound($event)"
+    >
+      <template v-slot:choice>You chose {{ round.choice }}</template>
+      <template v-slot:coin>The coin landed on {{ round.coin }}</template>
+    </round-detail>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RoundDetail from "./components/RoundDetail.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    RoundDetail
+  },
+  data: function() {
+    return {
+      flipped: false,
+      choice: "heads",
+      sides: ["heads", "tails"],
+      rounds: [],
+      round: 1
+    };
+  },
+  methods: {
+    flip: function() {
+      this.flipped = true;
+      this.coin = this.sides[Math.floor(Math.random() * 2)];
+
+      let winner = "";
+      if (this.choice == this.coin) {
+        winner = "Player";
+      } else {
+        winner = "Computer";
+      }
+
+      this.rounds.push({
+        number: this.round++,
+        winner: winner,
+        coin: this.coin,
+        choice: this.choice
+      });
+    },
+    deleteRound: function(roundNumber) {
+      function isMatchingRound(round) {
+        return round.number != this;
+      }
+      //console.log("Invoked deleteRound from root component");
+      this.rounds = this.rounds.filter(isMatchingRound, roundNumber);
+    }
   }
 };
 </script>
---
+++ week8spa/src/components/RoundDetail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-bind:class="{ winner : winner == 'Player'}">
+    <ul>
+      <li>Round number: {{ number }}</li>
+      <li>Winner: {{ winner }}</li>
+      <li>
+        <slot name="choice"></slot>
+      </li>
+      <li>
+        <slot name="coin"></slot>
+      </li>
+    </ul>
+    <button @click="$emit('delete-round', number)">Remove Round Test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  props: {
+    number: {
+      type: Number,
+      default: 0
+    },
+    winner: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    // this is a NO NO: you don't want to alter the parent directly from child instances
+    // shown for example only
+    deleteRound() {
+      console.log(this.$root.rounds);
+    }
+  },
+  template: "#round-detail"
+};
+</script>
+
+<style scoped>
+ul {
+  background-color: red;
+}
+</style>
