5dcd930e78d81283e7323a51748fbfb8cdc3cf25
Gerald Derrick <jerry.derrick@gmail.com>
2019-10-27 13:44:26 -0500

    Initial Project 2 File Upload

---
+++ p2/src/script.js
@@ -0,0 +1,13 @@
+//import Vue from 'vue';
+//import RoundDetail from './components/RoundDetail.vue';
+
+// root Vue instance
+let app = new Vue({
+    el: '#app',
+    components: {
+    },
+    data: {
+    },
+    methods: {
+    }
+});
