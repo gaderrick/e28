4ffa25283cf044882fc7c9f667cd5c507b4e1b9a
Jerry Derrick <jerry.derrick@gmail.com>
2019-11-11 20:08:02 -0500

    Added the featured recipe functionality

---
+++ p3/src/components/ShowFeatured.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <li v-for='recipe in featuredRecipes' :key='recipe.id'>{{ recipe.name }}</li>
+    </div>
+</template>
+<script>
+import * as app from './../app.js';
+
+export default {
+    name: 'ShowFeatured',
+    components: {},
+    props: ['id'],
+    computed: {
+        featuredRecipes: function() {
+            function isMatch(recipe) {
+                return recipe.id == this;
+            }
+            return this.sharedState.recipes.filter(isMatch, this.id);
+        }
+    },
+    data: function() {
+        return {
+            sharedState: app.site
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
--- p3/src/components/pages/HomePage.vue
+++ p3/src/components/pages/HomePage.vue
@@ -3,17 +3,24 @@
         <span>There are currently {{ sharedState.recipes.length }} recipes in the database.</span>
         <br />
         <br />
+        <span>This month's featured recipes</span>
+        <br />
+        <br />
+        <show-featured :id='1'></show-featured>
+        <show-featured :id='3'></show-featured>
     </div>
 </template>
 <script>
 import * as app from './../../app.js';
+import ShowFeatured from './../ShowFeatured.vue';
 
 export default {
     name: 'HomePage',
-    components: {},
+    components: { ShowFeatured },
     data: function() {
         return {
-            sharedState: app.site
+            sharedState: app.site,
+            featured: [1, 3]
         };
     },
     mounted() {}
